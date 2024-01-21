e565e13886a792c7912e3a51995f6164b90d01ca
a-pashinin <a-pashinin@yandex-team.ru>
2024-01-21 18:04:49 +0100

    added json for callback data

--- src/utils/Commons.py
+++ src/utils/Commons.py
@@ -1,11 +1,18 @@
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 
+from model.CallbackData import CallbackData, CallbackType
+
 
 def poll_reply_markup(options: "list[str]") -> InlineKeyboardMarkup:
-    book_buttons = [[InlineKeyboardButton(b, callback_data=str(b))]
-         for i, b in enumerate(options)]
-    not_reading_button = [[InlineKeyboardButton('Не читаю', callback_data=-1)]]
+    def option_to_button(option: str) -> InlineKeyboardButton:
+        callback: str = CallbackData(CallbackType.BOOK_VOTE, {
+                                     "book": option}).to_json()
+        return InlineKeyboardButton(option, callback_data=callback)
+    book_buttons = [[option_to_button(option)]
+                    for i, option in enumerate(options)]
+    not_reading_button = [[InlineKeyboardButton(
+        'Не читаю', callback_data=CallbackData(CallbackType.NOT_READING).to_json())]]
     return InlineKeyboardMarkup(
         # should use better callback_data
-        book_buttons+not_reading_button 
+        book_buttons + not_reading_button
     )
